0df0dcfe775d23d8c750b11c7b8f267c82bcb9f7
Shanthan Cheruku <shanthanc@gmail.com>
2023-03-14 14:41:50 -0500

    first commit

---
+++ src/main/java/com/shanthan/ibowebservice/IboWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shanthan.ibowebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IboWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IboWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shanthan/ibowebservice/IboWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanthan.ibowebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IboWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
